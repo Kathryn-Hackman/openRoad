d25353df1fe0553a107bedb99b586bc2f0841d7c
Kathryn-Hackman <hackmank@bu.edu>
2019-11-04 17:19:48 -0500

    cooler button

--- pythonparameterhandling.py
+++ pythonparameterhandling.py
@@ -1,1 +1,8 @@
 #takes user-inputted parameters start and end point (as adresses), then hits the google maps api to calculate how far they are away from each other (driving time), then returns that as a 'Journey' object of type JSON to the front end, where it is displayed as start address, end address, and driving time
+import googlemaps
+from datetime import datetime
+
+gmaps = googlemaps.Client(key= 'our api key')
+#generates driving directions by default
+#value of start and end address should be imported from textinput.js
+directions_result = gmaps.directions(start_address, end_address)
--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
+import TextInputMultiline from '../TextInput';
 
 export default function LinksScreen() {
   return (
     <ScrollView style={styles.container}>
-      {/**
-       * Go ahead and delete ExpoLinksView and replace it with your content;
-       * we just wanted to provide you with some helpful links.
-       */}
+      <TextInputMultiline placeholderText={'Start Address'}/>
+      <TextInputMultiline placeholderText={'End Address'}/>
+      <Buttton title = "andiamo!" onPress={() => navigate('Links')} Button/>
     </ScrollView>
   );
 }
-
+//button onpress - send to backend
 LinksScreen.navigationOptions = {
   title: 'user input',
 };
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
+    paddingTop: 30,
     backgroundColor: '#fff',
   },
 });
