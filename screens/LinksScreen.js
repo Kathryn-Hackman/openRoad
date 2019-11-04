34d127bc9436d3db3661ae038cd0027005cec659
jimswe1239 <33169224+jimswe1239@users.noreply.github.com>
2019-11-04 17:13:13 -0500

    Update LinksScreen.js

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
+import { TextInputBox } from '../components/TextInputBox';
+
+var url = "";//manually enter your ip address
+url = 'http://'+ url  +':5000/'
+
+var startTest = "address 1"
+var endTest = "address 2"
+var name = "New Journey 7"
+
+  
+function createNewJourney(start,end) {
+  
+console.log("Creating New Journey...");
+fetch(
+	url+'newJourney?start='+encodeURIComponent(start)+"&end="+encodeURIComponent(end)+"&name="+encodeURIComponent(name)
+)
+	.then(res => res.json())
+	.then(json => {
+		console.log("Call response:");
+		console.log(json);
+		console.log("New Journey Created.");
+	});
+}
 
 export default function LinksScreen() {
   return (
     <ScrollView style={styles.container}>
-      {/**
-       * Go ahead and delete ExpoLinksView and replace it with your content;
-       * we just wanted to provide you with some helpful links.
-       */}
+      	<View>
+					<TextInputBox label='Start'></TextInputBox>
+					<TextInputBox label='Destination'></TextInputBox>
+					
+					<Text style={styles.getStartedText} onPress={() => createNewJourney(startTest,endTest)}>
+					Create New Journey!
+					</Text>
+				</View>
     </ScrollView>
   );
 }
