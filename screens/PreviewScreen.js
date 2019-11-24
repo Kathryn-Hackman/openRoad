479679cbc234e116aac536fce37cfc3dc9c73995
Kathryn-Hackman <hackmank@bu.edu>
2019-11-24 17:39:52 -0500

    added markers to map

--- screens/PreviewScreen.js
+++ screens/PreviewScreen.js
@@ -4,6 +4,7 @@ import MapView from 'react-native-maps';
 import { ExpoLinksView } from '@expo/samples';
 import { TextInputBox } from '../components/TextInputBox';
 import { NavigationEvents } from 'react-navigation';
+import  { Marker } from 'react-native-maps';
 
 //to get the user's current location
 //Geolocation.getCurrentPosition(info => console.log(info));
@@ -11,14 +12,20 @@ import { NavigationEvents } from 'react-navigation';
 export default function PreviewScreen(){
       return (
         <MapView
-        style ={styles.map}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude:-122.4324,
-          latitudeDelta:0.0922,
-          longitudeDelta:.0421,
-        }}
-        />
+          style ={styles.map}
+          region={{
+            latitude: 42.34817,
+            longitude:-71.106972,
+            latitudeDelta:0.0922,
+            longitudeDelta:.0421,
+          }}
+        >
+          <Marker style = {styles.mapMarker}
+            coordinate={{latitude:42.34817, longitude:-71.106972}}
+            title={"First Marker"}
+            description={"this is the first marker"}
+          />
+        </MapView>
   ); 
 }
 
